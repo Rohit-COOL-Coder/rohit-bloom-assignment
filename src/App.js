3d0e7246457a042383633bc82fee1bb1e50c4ccc
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-16 20:56:38 +0530

    work completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
+import { useState } from "react";
 import styled from "styled-components";
+import app from './firebase'
+import{ getStorage,ref,uploadBytesResumable,getDownloadURL} from 'firebase/storage'
 
 const Wrapper=styled.div``
 const FomeContainer=styled.div``
-const Button=styled.button``
+const Button=styled.button`
+cursor: pointer;
+`
 function App() {
 
   const [file,setFile]=useState(null)
 
+  const handleSubmit= (e)=>{
+    e.preventDefault()
+    
+    //file upload
+
+  const fileName=new Date().getTime() + file.name
+  const storage = getStorage(app);
+  const storageRef = ref(storage, fileName);
+
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed', 
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+
+                break;
+                default:
+            }
+          }, 
+          (error) => {
+            console.log("Handle unsuccessful uploads")
+          }, 
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+            alert("Use this link to view your file" ,downloadURL)
+            });
+          }
+        );
+}
+
   return (
     <Wrapper>
       <FomeContainer>
-        <input type="file"/>
-        <Button>Submit</Button>
+        <input type="file"  onChange={(e)=>setFile(e.target.files[0])}/>
+        <Button onClick={handleSubmit}>Submit</Button>
       </FomeContainer>
     </Wrapper>
+
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAA_O6SXxxRcZN73rELdLxRX6Y69G-ItLA",
-//   authDomain: "bloom-40b41.firebaseapp.com",
-//   projectId: "bloom-40b41",
-//   storageBucket: "bloom-40b41.appspot.com",
-//   messagingSenderId: "454577820445",
-//   appId: "1:454577820445:web:48cb35618098ba6eaa47f4"
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAA_O6SXxxRcZN73rELdLxRX6Y69G-ItLA",
+  authDomain: "bloom-40b41.firebaseapp.com",
+  projectId: "bloom-40b41",
+  storageBucket: "bloom-40b41.appspot.com",
+  messagingSenderId: "454577820445",
+  appId: "1:454577820445:web:48cb35618098ba6eaa47f4"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
