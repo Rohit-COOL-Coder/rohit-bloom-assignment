b5c6c925a87dd33174a54d877558e259e2070f62
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-16 20:42:20 +0530

    stated workin bloom assmnt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
+const Wrapper=styled.div``
+const FomeContainer=styled.div``
+const Button=styled.button``
 function App() {
+
+  const [file,setFile]=useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <FomeContainer>
+        <input type="file"/>
+        <Button>Submit</Button>
+      </FomeContainer>
+    </Wrapper>
   );
 }
 
